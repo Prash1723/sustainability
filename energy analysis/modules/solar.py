8b6910c2ebf6ce6f76ab7f8085df392b0a1ef7ce
p <s.praveen795@gmail.com>
2023-05-22 19:19:24 +0530

    Module change
    Module design change

--- energy analysis/modules/solar.py
+++ energy analysis/modules/solar.py
@@ -4,25 +4,32 @@
 
 orientation = ['north', 'south', 'east', 'west']
 
-def pv_daily(hours, watts):
-	return hours * watts * 0.75
+class solar:
+    def __init__(self, hours, watts, builtup_area, numbers):
+        self.hours = hours
+        self.watts = watts
+        self.builtup_area = builtup_area
+        self.numbers = numbers
 
-def tilt_angle(month, latitude):
-	if month == 'summer':
-		ang = latitude - 15
-	else:
-		ang = latitude + 15
+    def pv_daily(hours, watts):
+    	return hours * watts/1000 * 0.75
 
-	return ang
+    def tilt_angle(month, latitude):
+        if month == 'summer':
+            ang = latitude-15
+        else:
+            ang = latitude+15
 
-def tilt_angle2(month, latitude):
-	if month == 'summer':
-		ang = (latitude * 0.9) + 29
-	elif month == 'winter':
-		ang = (latitude * 0.9) - 29
-	elif month == 'fall':
-		ang = ((latitdue - 2.5) * 0.9) + 29
-	else:
-		ang = ((latitude - 2.5) * 0.9) - 29
+        return ang
 
-	return ang
+    def tilt_angle2(month, latitude):
+        if month == 'summer':
+            ang = (latitude * 0.9) + 29
+        elif month == 'winter':
+            ang = (latitude * 0.9) - 29
+        elif month == 'fall':
+            ang = ((latitude - 2.5) * 0.9) + 29
+        else:
+            ang = ((latitude - 2.5) * 0.9) - 29
+
+        return ang
--- energy analysis/test.py
+++ energy analysis/test.py
@@ -1,5 +1,27 @@
-from modules import solar
+from modules.solar import solar
+from rich.console import Console
 
-print(solar.pv_daily(5,1000), "kWh")
+rc = Console()
 
-print(solar.tilt_angle("summer", 34), "degrees")
+rc.log(solar.pv_daily(5,1000), "kWh")
+
+rc.log(solar.tilt_angle("summer", 34), "degrees")
+
+rc.log(round(solar.tilt_angle2("winter", 34), 2), "degrees")
+
+energy_consumption = {
+	'jan': 87,
+	'dec': 78,
+	'nov': 113,
+	'oct': 170,
+	'sep': 176,
+	'aug': 165,
+	'jul': 215,
+	'jun': 429,
+	'may': 504,
+	'apr': 510,
+	'mar': 267,
+	'feb': 195
+}
+
+
